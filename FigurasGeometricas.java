49ff061200ae23bf5fece0cbae763df8c8e349d5
Florpr <floralondraguadalupe@gmail.com>
2025-09-18 11:19:27 -0500

    Add files via upload

---
+++ FigurasGeometricas.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+// File: FigurasGeometricas.java
+// Author: Flor Guadalupe y Evan Mendez
+// Date: 2025-03-25
+// Description: User sees the out as different Geometric Figures.
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("In the following there will be 4 different geometric figures shown: ");
+        System.out.println("Here is a triangle: ");
+ System.out.println("*");
+ System.out.println("**");
+ System.out.println("***");
+ System.out.println("****");
+ System.out.println("*****");
+        System.out.println("Here is a empty rectangle: ");
+        System.out.println("******************");
+        System.out.println("*                *");
+        System.out.println("*                *");
+        System.out.println("*                *");
+        System.out.println("******************");
+        System.out.println("Here is a full rectangle: ");
+        System.out.println("################");
+        System.out.println("################");
+        System.out.println("################");
+        System.out.println("################");
+        System.out.println("Here is a diamond: ");
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+
+    }
+}
