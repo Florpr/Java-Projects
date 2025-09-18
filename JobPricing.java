e01c92d02b62d15b9870e951ad6d66187fc14301
Florpr <floralondraguadalupe@gmail.com>
2025-09-18 11:20:28 -0500

    Add files via upload

---
+++ JobPricing.java
@@ -0,0 +1,19 @@
+//File:JobPricing
+//Author: Flor A. Guadalupe y Evan Mendez
+//Date: 3-11-2025
+//Class: Structured Programming
+//Description:
+
+import java.util.Scanner;
+
+
+public class JobPricing {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Job name: ");
+        System.out.println("Enter cost of material: ");
+        System.out.println("Enter the number of hours worked: ");
+        System.out.println("Enter the number of hours travel time: ");
+    }
+}
